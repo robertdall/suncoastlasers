549382fd8ce781df62ede0dc96e6db559d993a57
Robert Dall <robert@robertdall.com>
2015-07-05 14:04:36 -0700

    Adds Sidebar style to Front page first pass

--- functions.php
+++ functions.php
@@ -72,13 +72,13 @@ function blm_widgets_init() {
 		'after_title' => '</h4>'
 	) );
 	register_sidebar( array(
-		'id' => 'front-page',
+		'id' => 'front-sidebar',
 		'name' => __( 'Front Page', 'blm_basic' ),
 		'description' => __( 'The following widgets will appear on the front page div.', 'blm_basic' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>',
-		'before_title' => '<h4>',
-		'after_title' => '</h4>'
+		'before_title' => '<h3>',
+		'after_title' => '</h3>'
 	) );
 }
 add_action( 'widgets_init', 'blm_widgets_init' );
