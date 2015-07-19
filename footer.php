2d4a875237fc0de1e7439159a9dd4a201df014ac
Robert Dall <robert@robertdall.com>
2015-07-18 17:27:16 -0700

    Adds better footer copyright and credit notices

--- footer.php
+++ footer.php
@@ -14,10 +14,11 @@
 								) ); ?>
 		</div>
 		<div id="footer-right">
-			<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/sailing-laser-class-symbol.png" alt="Laser Sailing Class Symbol" /></a>
+			<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/sailing-laser-class-symbol-200.png" alt="Laser Sailing Class Symbol" /></a>
 		</div>
 		
-			<p class="copyright">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?> &ndash; Web Design <a href="http://robertdall.com/">Robert Dall</a> </p>
+			<p class="copyright">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?> </p>
+			<p class="copyright"> </p> Web Design <a href="http://robertdall.com/">Robert Dall</a> Hosting Provided by <a rel="nofollow" href="http://www.site5.com/in.php?id=73296">Site5</a> Powered by <a href="http://wordpress.org">WordPress</a> and the wind.</p>
 		</div> <!-- #wrap --> 
 	</footer>
 	
