1da7458ec3427e725a84b20a570a8c64d80952ba
Robert Dall <robert@robertdall.com>
2015-07-25 12:13:17 -0700

    Adds Laser class symbol to footer

--- footer.php
+++ footer.php
@@ -7,15 +7,11 @@
 ?>
 	<footer id="footer">
 		<div class="wrap">
-		 <div id="footer-left">
 			<h4><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h4>
 			<?php wp_nav_menu( array(	'theme_location' => 'footer', 
 										'menu_class' => 'footer-menu' 
 								) ); ?>
-		</div>
-		<div id="footer-right">
-			<a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/sailing-laser-class-symbol-200.png" alt="Laser Sailing Class Symbol" /></a>
-		</div>
+			<img class="laser-symbol" src="<?php echo get_template_directory_uri(); ?>/images/sailing-laser-class-symbol-200.png" alt="Laser Sailing Class Symbol" />
 		
 			<p class="copyright">&copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?> </p>
 			<p class="copyright">Web Design <a href="http://robertdall.com/">Robert Dall</a> Hosting Provided by <a rel="nofollow" href="http://www.site5.com/in.php?id=73296">Site5</a> Powered by <a href="http://wordpress.org">WordPress</a> and the wind.</p>
