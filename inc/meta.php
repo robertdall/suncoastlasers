f8313f92f4f8e2951b849eb1f989e839e19067c0
Robert Dall <robert@robertdall.com>
2015-08-01 10:39:38 -0700

    Comment out Entry Meta But Keeping Template
    Incase we decide to implement later.

--- inc/meta.php
+++ inc/meta.php
@@ -7,7 +7,10 @@
  * @package blm_basic
  */
 ?>
+<?php /*
 <div class="entrymeta">
 	<p>Written by <?php the_author();?>, <?php the_time( 'F dS, Y' ); ?> | <?php comments_popup_link( 'No Comments &#187;', '1 Comment &#187;', '% Comments &#187;' ); ?></p>
 	<p>Filed under: <?php the_category(', ');?> | <?php echo the_tags(); ?> </p>
 </div>
+
+*/ ?>
